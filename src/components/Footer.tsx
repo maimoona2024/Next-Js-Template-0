540ffcb17e4dcf65af1a23f83cae631c2cd3e4dc
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-12-10 01:23:33 -0800

    second commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { LuSendHorizontal } from "react-icons/lu";
-import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer className="bg-white text-black">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-5 gap-8 py-10 px-10">
         {/* Address Section */}
-        <div className="flex items-center">
-        400 University Drive Suite 200 Coral Gables,
-        FL 33134 USA
+        <div className="flex items-center gap-4">
+          
+          400 University Drive Suite 200 Coral Gables, FL 33134 USA
         </div>
 
         {/* Links Section */}
@@ -18,7 +17,6 @@ const Footer = () => {
           <p className="text-sm mb-2 font-extralight">About</p>
           <p className="text-sm mb-2 font-extralight">Shop</p>
           <p className="text-sm font-extralight">Contact</p>
-
         </div>
 
         {/* Help Section */}
@@ -31,22 +29,25 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Quick Link Section */}
+        {/* Newsletter Section */}
         <div>
-          <h2 className="text-lg font-bold mb-4 text-gray-500">News Letter</h2>
-          <div  className="flex gap-2">
-          <input type="text"  placeholder="Enter Your Email Address" className=" h-[40px] w-[286px] bg-gray-200 rounded-md p-1"/>
-          <button className="text-[18px] font-light text-gray-500">SUBSCRIBE</button>
+          <h2 className="text-lg font-bold mb-4 text-gray-500">Newsletter</h2>
+          <div className="flex gap-2 items-center">
+            <input
+              type="text"
+              placeholder="Enter Your Email Address"
+              className="h-[40px] w-[286px] bg-gray-200 rounded-md p-1"
+            />
+            <button className="flex items-center gap-1 text-[18px] font-light text-gray-500">
+              SUBSCRIBE
+            </button>
           </div>
-          
         </div>
-
-        
       </div>
 
       {/* Footer Bottom */}
       <div className="flex justify-center items-center border-t border-gray-300 font-extralight text-sm h-12">
-        <p>2022 Meubel House. All rights reverved</p>
+        <p>2022 Meubel House. All rights reserved</p>
       </div>
     </footer>
   );
