2601484a7453d679cd5f4581a6add49307171066
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-12-12 11:01:01 -0800

    first commit

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,40 +1,55 @@
+"use client"
+import { useState } from 'react'
 import React from 'react'
 import Image from 'next/image'
 import { CiFilter } from "react-icons/ci";
 import { MdGridView } from "react-icons/md";
 import { MdOutlineCalendarViewDay } from "react-icons/md";
+import { FiMenu } from 'react-icons/fi';
 
 const shop = () => {
+    let name;
+    const [open, setOpen] = useState(false)
+    const [seo, setSeo] = useState("Test")
     return (
         <div>
             <div>
                 <Image src={"/shop.png"} width={1440} height={36} alt=''></Image>
             </div>
-            <div className='bg-[#f0dbdb] w-full flex-col sm:flex sm:flex-row items-center gap-4 px-8 py-4'>
-                <div className='flex items-center gap-4'>
-                    <CiFilter />
-                    <p>Filter</p>
+            <header className="bg-[#FBEBB5] py-2 w-full flex itemsce ">
+                <div className="flex justify-between items-center xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
+                    <FiMenu className="lg:hidden block h-6 w-6 cursor-pointer" onClick={() => setOpen(!open)} />
+                    <nav className={`${open ? "block" : "hidden"} lg:flex lg:items-center lg:w-auto w-full flex flex-col lg:flex-row items-center`}>
+                        <ul className="text-base w-full text-black lg:flex lg:justify-between flex flex-col lg:flex-row gap-4">
+                            <li className="px-4 flex justify-center md:items-center gap-2 lg:px-5 py-2 hover:text-gray-800 font-medium cursor-pointer">
+                                <CiFilter />
+                                <p>Filter</p>
+                            </li>
+                            <li className="px-4 flex  justify-center md:items-center hover:text-gray-800 font-medium cursor-pointer">
+                                <MdGridView />
+                            </li>
+                            <li className="px-4 flex  justify-center md:items-center hover:text-gray-800 font-medium cursor-pointer">
+                                <MdOutlineCalendarViewDay />
+                            </li>
+                            <li className="px-4 flex  justify-center md:items-center hover:text-gray-800 font-medium cursor-pointer">
+                                <p>Showing 1–16 of 32 results</p>
+                            </li>
+                            <li className='font-[sans-serif] w-max mx-auto  rounded-lg flex  lg:flex-row flex-col p-3 cursor-pointer sm:justify-center items-center'>
+                                <button type="button" className="px-6 py-3 border-black border-2 bg-white border-b-0 lg:border-none text-black text-sm font-semibold outline-none hover:bg-gray-200 active:bg-gray-300">Show</button>
+                                <button type="button" className="px-6 py-3 border-black border-2 lg:border-none w-[88px] lg:w-auto bg-white text-black text-sm font-semibold outline-none hover:bg-gray-200 active:bg-gray-300">16</button>
+                                <div className='text-[#FBEBB5]'>
+                                    Hello
+                                </div>
+                                <button type="button" className="px-5 py-3 lg:px-3 lg:w-32 border-black
+                                 border-2 border-b-0 lg:border-none bg-white text-black text-sm font-semibold outline-none hover:bg-gray-200 active:bg-gray-300">Sort By</button>
+                                <button type="button" className="px-5 py-3 lg:px-6 border-black border-2 lg:border-none bg-white text-black text-sm font-semibold outline-none hover:bg-gray-200 active:bg-gray-300">Default</button>
+
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <div>
-                    <MdGridView />
-                </div>
-                <div>
-                    <MdOutlineCalendarViewDay />
-                </div>
-                <div className='border border-l-2 border-black border-none'>
-                    <p>Showing 1–16 of 32 results</p>
-                </div>
-                {/* Button Group 1 */}
-                <div className="font-[sans-serif] w-max mx-auto  rounded-lg flex sm:flex-row flex-col">
-                    <button type="button" className="px-6 py-3 bg-white text-black text-sm font-semibold border-none outline-none hover:bg-gray-200 active:bg-gray-300">Show</button>
-                    <button type="button" className="px-6 py-3 bg-white text-black text-sm font-semibold border-none outline-none hover:bg-gray-200 active:bg-gray-300">16</button>
-                    <div className='text-[#f0dbdb]'>
-                        Hello
-                    </div>
-                    <button type="button" className="px-6 py-3 bg-white text-black text-sm font-semibold border-none outline-none hover:bg-gray-200 active:bg-gray-300">Sort By</button>
-                    <button type="button" className="px-6 py-3 bg-white text-black text-sm font-semibold border-none outline-none hover:bg-gray-200 active:bg-gray-300">Default</button>
-                </div>
-            </div>
+
+            </header>
 
 
             <section>
